058a2c31320b6fd3fd4a1a55b4b864434f5102b6
palocziimi <palocziimi@gmail.com>
2024-09-25 10:10:46 +0200

    done for now

--- desserts-frontend/src/components/DessertCard.tsx
+++ desserts-frontend/src/components/DessertCard.tsx
@@ -1,18 +1,21 @@
 export type DessertCardProp = {
-    name: string,
-    category: string,
-    price: number,
-    image?: {
-        thumnail?: string,
-        mobile?: string,
-        tablet?: string,
-        desktop: string,
-    },
+  name: string,
+  category: string,
+  price: number,
+  image?: {
+    thumnail?: string,
+    mobile?: string,
+    tablet?: string,
+    desktop: string,
+  },
 }
 
 const DessertCard = (props: DessertCardProp) => {
   return (
-    <div>{props.name}</div>
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.image?.desktop} alt="kep" />
+    </div>
   )
 }
 
--- desserts-frontend/src/pages/home.tsx
+++ desserts-frontend/src/pages/home.tsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from "react";
 import DessertCard from "../components/DessertCard"
 import { DessertCardProp } from "../components/DessertCard";
 
 const home = () => {
-    let lista: DessertCardProp = [
-        {name:"Almáspote", category:"Pie", price:0},
-    ];
+    // let lista: DessertCardProp[] = [
+    //     {name:"Almáspote", category:"Pie", price:0},
+    // ];
+
+    const [desserts, setDesserts] = useState<DessertCardProp[]>([])
+
+    useEffect(() => {
+        fetch("/data.json")
+        .then(res => res.json())
+        .then(apiDesserts => setDesserts(apiDesserts))
+    }, [])
 
     return (
         <div>
@@ -12,7 +21,7 @@ const home = () => {
                 <h1>Desserts </h1>
                 <div className="cards-grid">
                     {
-                        lista.map((suti) => <DessertCard {...suti}/>)
+                        desserts.map((dessert) => <DessertCard {...dessert}/>)
                     }
                 </div>
             </section>
